fae374ada8836f1ad7855729eef51c3ebc52fea1
uioppoiu <hongsik.lee86@gmail.com>
2020-12-05 16:36:18 +0900

    1205

--- app.ino
+++ app.ino
@@ -6,41 +6,34 @@
 
 void onRequestGet(uint32_t seqId, const UartMessageInterface::RequestGetData* dataArr, size_t arrSize)
 {
-    Serial.println(__FUNCTION__);
-    for(size_t arrIdx = 0 ; arrIdx < arrSize ; arrIdx++)
-    {
-        const UartMessageInterface::RequestGetData& data = dataArr[arrIdx];
-        Serial.print("SeqId:");
-        Serial.print(seqId);
-        Serial.print(" Type:");
-        Serial.print((uint32_t)data.type);
-        Serial.print(" Name:");
-        Serial.println(data.name);
-    }
-
-    UartMessageInterface::UartMessageSender rspGet(UartMessageInterface::MsgId::ResponseGet);
-    rspGet.setSeqId(seqId+10000);
-    rspGet.appendResponseGetData(UartMessageInterface::DataType::SensorHumidity, "TENT", sizeof("TENT"), 321);
-    rspGet.appendResponseGetData(UartMessageInterface::DataType::SensorCO2, "ROOM", sizeof("ROOM"), 4321);
-    rspGet.appendResponseGetData(UartMessageInterface::DataType::SensorTemperature, "WATER", sizeof("WATER"), 54321);
-    rspGet.sendMessage();
+    // Serial.println(__FUNCTION__);
+    // for(size_t arrIdx = 0 ; arrIdx < arrSize ; arrIdx++)
+    // {
+    //     const UartMessageInterface::RequestGetData& data = dataArr[arrIdx];
+    //     Serial.print("SeqId:");
+    //     Serial.print(seqId);
+    //     Serial.print(" Type:");
+    //     Serial.print((uint32_t)data.type);
+    //     Serial.print(" Name:");
+    //     Serial.println(data.name);
+    // }
 }
 
 void onResponseGet(uint32_t seqId, const UartMessageInterface::ResponseGetData* dataArr, size_t arrSize)
 {
-    Serial.println(__FUNCTION__);
-    for(size_t arrIdx = 0 ; arrIdx < arrSize ; arrIdx++)
-    {
-        const UartMessageInterface::ResponseGetData& data = dataArr[arrIdx];
-        Serial.print("SeqId:");
-        Serial.print(seqId);
-        Serial.print(" Type:");
-        Serial.print((uint32_t)data.type);
-        Serial.print(" Name:");
-        Serial.print(data.name);
-        Serial.print(" Value:");
-        Serial.println(data.value);
-    }
+    // Serial.println(__FUNCTION__);
+    // for(size_t arrIdx = 0 ; arrIdx < arrSize ; arrIdx++)
+    // {
+    //     const UartMessageInterface::ResponseGetData& data = dataArr[arrIdx];
+    //     Serial.print("SeqId:");
+    //     Serial.print(seqId);
+    //     Serial.print(" Type:");
+    //     Serial.print((uint32_t)data.type);
+    //     Serial.print(" Name:");
+    //     Serial.print(data.name);
+    //     Serial.print(" Value:");
+    //     Serial.println(data.value);
+    // }
 }
 
 void onNotification(uint32_t seqId, const UartMessageInterface::NotificationData* dataArr, size_t arrSize)
@@ -78,7 +71,7 @@ void onAcknowledge(uint32_t seqId, unsigned char msgId)
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
     Serial.println("Serial OK...");
     Serial.flush();
 
@@ -101,65 +94,75 @@ void setup()
 // the loop function runs over and over again forever
 void loop()
 {
-    // digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-    // delay(500);                     // wait for a second
-    // digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-    // delay(500);                     // wait for a second
+    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+    delay(1000);                     // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+    delay(1000);                     // wait for a second
+
+    static uint32_t seqId = 10000;
+    UartMessageInterface::UartMessageSender rspGet(UartMessageInterface::MsgId::ResponseGet);
+    rspGet.setSeqId(seqId++);
+    rspGet.appendResponseGetData(UartMessageInterface::DataType::SensorHumidity, "TENT", sizeof("TENT"), 321);
+    rspGet.appendResponseGetData(UartMessageInterface::DataType::SensorCO2, "ROOM", sizeof("ROOM"), 4321);
+    rspGet.appendResponseGetData(UartMessageInterface::DataType::SensorTemperature, "WATER", sizeof("WATER"), 54321);
+    rspGet.appendResponseGetData(UartMessageInterface::DataType::SensorTemperature, "ROOM", sizeof("ROOM"), 98765);
+    rspGet.appendResponseGetData(UartMessageInterface::DataType::SensorConductivity, "Point1", sizeof("Point1"), seqId * 10);
+    rspGet.sendMessage();
 }
 
-uint8_t readBuffer[128] = {0,};
-size_t readBufferIdx = 0;
-void serialEvent()
-{
-    while (Serial.available() > 0)
-    {
-        readBuffer[readBufferIdx++] = (uint8_t)Serial.read();
+// uint8_t readBuffer[128] = {0,};
+// size_t readBufferIdx = 0;
+// void serialEvent()
+// {
+//     while (Serial.available() > 0)
+//     {
+//         readBuffer[readBufferIdx++] = (uint8_t)Serial.read();
 
-        bool isBegin = false;
-        if (readBufferIdx >= 7)
-        {
-            if (memcmp(readBuffer + readBufferIdx - 7, "<BEGIN>", 7) == 0)
-            {
-                isBegin = true;
-            }
-        }
+//         bool isBegin = false;
+//         if (readBufferIdx >= 7)
+//         {
+//             if (memcmp(readBuffer + readBufferIdx - 7, "<BEGIN>", 7) == 0)
+//             {
+//                 isBegin = true;
+//             }
+//         }
 
-        if (isBegin)
-        {
-            Serial.println("BEGIN FOUND!!");
+//         if (isBegin)
+//         {
+//             // Serial.println("BEGIN FOUND!!");
 
-            memset(readBuffer, 0x00, sizeof(readBuffer));
-            readBufferIdx = 0;
-            continue;
-        }
+//             memset(readBuffer, 0x00, sizeof(readBuffer));
+//             readBufferIdx = 0;
+//             continue;
+//         }
 
-        bool isEnd = false;
-        if (readBufferIdx >= 5)
-        {
-            if (memcmp(readBuffer + readBufferIdx - 5, "<END>", 5) == 0)
-            {
-                isEnd = true;
-            }
-        }
+//         bool isEnd = false;
+//         if (readBufferIdx >= 5)
+//         {
+//             if (memcmp(readBuffer + readBufferIdx - 5, "<END>", 5) == 0)
+//             {
+//                 isEnd = true;
+//             }
+//         }
 
-        if (isEnd)
-        {
-            Serial.println("END FOUND!!");
+//         if (isEnd)
+//         {
+//             // Serial.println("END FOUND!!");
 
-            readBufferIdx = readBufferIdx - 5;
-            UartMessageInterface::UartMessageReceiver rcv(readBuffer, readBufferIdx);
-            rcv.processMessage();
+//             readBufferIdx = readBufferIdx - 5;
+//             UartMessageInterface::UartMessageReceiver rcv(readBuffer, readBufferIdx);
+//             rcv.processMessage();
 
-            memset(readBuffer, 0x00, sizeof(readBuffer));
-            readBufferIdx = 0;
+//             memset(readBuffer, 0x00, sizeof(readBuffer));
+//             readBufferIdx = 0;
 
-            continue;
-        }
+//             continue;
+//         }
 
-        if(readBufferIdx == 256)
-        {
-            memset(readBuffer, 0x00, sizeof(readBuffer));
-            readBufferIdx = 0;
-        }
-    }
-}
+//         if(readBufferIdx == 128)
+//         {
+//             memset(readBuffer, 0x00, sizeof(readBuffer));
+//             readBufferIdx = 0;
+//         }
+//     }
+// }
